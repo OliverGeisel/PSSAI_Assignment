5ef01721f0d4b2fde075a3b42296d7b4b7225233
Oliver Geisel <geisel.oliver@googlemail.com>
2020-01-21 09:10:29 +0100

    Add copy of schedule and more infos about timeStep

--- src/schedule.py
+++ src/schedule.py
@@ -1,15 +1,18 @@
 from typing import List
-
+import copy
 from src.job import Job
 from src.machine import Machine
 
 
 class Schedule:
 
-    def __int__(self, jobs: List[Job], machines: List[Machine]):
+    def __init__(self, jobs: List[Job], machines: List[Machine]):
         self.num_of_machines = len(machines)
         self.machines = machines
         self.jobs = jobs
         time_list = [x.min_time for x in jobs]
         self.min_time = min(time_list)
         self.max_time = sum(time_list)
+
+    def copy(self):
+        return copy.deepcopy(self)
--- src/timeStep.py
+++ src/timeStep.py
@@ -1,5 +1,10 @@
-class TimeStep:
-    pass
+from src.job import Step, Job
 
-    def __init__(self, type: bool = False):
+
+class TimeStep:
+
+    def __init__(self, step: Step, step_number: int, job: Job, type: bool = False):
         self.type = type
+        self.step = step
+        self.step_number = step_number
+        self.job = job
