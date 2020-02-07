2f23fd65dcd90992e37f721c2d0897c2235d6534
Georg Arndt <jonnys.kiste@web.de>
2020-02-07 14:05:01 +0100

    Better solve and find out params and measure time

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import sys
+import time
 
 from src.decoder import parse_tasks
 from src.machine import Machine
 from src.schedule import Schedule
 from src.solver import solve
+
+# how many iterations should there be
+from_iteration = 0
+to_iterations = 100
+
+# how long should a moved step be blocked
+from_block_time = 0
+to_block_time = 1
+
+time_list = list()
+schedule_list = list()
+iter_param = list()
+block_param = list()
 
 
 def run(*args):
@@ -13,8 +27,41 @@ def run(*args):
         for i in range(task.machine_count):
             machines.append(Machine(i))
         schedule = Schedule(task.jobs, machines)
-        solve(schedule)
-        print("Fertig")
+
+        # try some parameters to compare wich is faster
+        for i in range(from_iteration, to_iterations):
+            for block in range(from_block_time, to_block_time):
+                start = time.time()
+                schedule_list.append(solve(schedule, i, block))
+                end = time.time()
+                time_list.append(end - start)
+                iter_param.append(i)
+                block_param.append(block)
+            print(f"Iteration with {i} Iterations is over")
+
+        shortest_schedule = min(schedule_list)
+        shortest_schedule_indizes = list()
+        # search for all the schedules that have same (minimal) length
+        for x in schedule_list:
+            if shortest_schedule == x:
+                shortest_schedule_indizes.append(schedule_list.index(x))
+
+        # list the times that the shortest schedules needed to compute
+        shortest_times = list()
+        for i in range(0, len(time_list) - 1):
+            if i in shortest_schedule_indizes:
+                shortest_times.append(time_list[i])
+
+        # get shortest needed time
+        shortest_time = min(shortest_times)
+        # get shortest schedule with smallest time needed
+        fastest_schedule_index = time_list.index(shortest_time)
+
+        print(f"Fastest Schedule drawn took {shortest_time}s " +
+              f"to calculate and had follwoing params:")
+        print(f"Iterations: {iter_param[fastest_schedule_index]}")
+        print(f"Block length: {block_param[fastest_schedule_index]}")
+        print(schedule_list[fastest_schedule_index])
 
 
 if __name__ == "__main__":
--- src/solver.py
+++ src/solver.py
@@ -1,90 +1,81 @@
+import random
+
 # from src.job import Step
 from src.schedule import Schedule
 from src.timeStep import idle_timeStep
 
-last_schedules = list()
 
-# how many schedules should be compared
-# nur sinnvoll, wenn wir wissen, wir bewegen uns in die richtige Richtung.
-# Ansonsten einfach alle behalten
-# make it greater than iterations to keep all
-stc = 20
-
-# how many iterations should there be
-iterations = 1000
-
-# how deep should a path you want to look into be
-path_deepness = 5
-
-# how long should a moved step be blocked
-block_time = 5
-
-
-def solve(schedule: Schedule):
+def solve(schedule: Schedule, iterations: int, block_time: int):
+    shortest_schedule = schedule.copy()
     for iteration in range(iterations):
-
-        # Es fehlen weitere Abbruchbedingungen
 
         # if all jobs are perfectly fitted return the schedule
         if schedule.check_perfect():
             return schedule
 
-        # if the length of schedules is going to be too long delete
-        # first element
-        if last_schedules is not None and len(last_schedules) >= stc:
-            last_schedules.pop(0)
-        # append the schedule to list of schedules to compare
-        last_schedules.append(schedule.copy())
+        if schedule < shortest_schedule:
+            shortest_schedule = schedule.copy()
 
-        # Vergleiche Längen der schedules
-        shortest_schedule = min(last_schedules)
-        if shortest_schedule.get_execute_time() <= schedule.min_time:
-            return shortest_schedule
-
-        # maybe unblock jobs
-
-        schedule = shortest_schedule
+        # unblock jobs
         schedule.reduce_block_count()
+
         # --------------- Begin of solving logic ---------------
 
-        # sort machines
-        machine_to_take_index = 0
-        # get longest job
-        longest_job = max(schedule.machines).work[-1].job
-        index = len(longest_job.steps) - 1
-        current_step = longest_job.steps[index]
-        # try new step for a better schedule
-        try_new_step(current_step, index, schedule)
+        # print(schedule)
+        # For the first time try switching steps of longest job
+        if iteration < 0.2 * iterations:
+            longest_job = max(schedule.machines).work[-1].job
+            index = len(longest_job.steps) - 1
+            current_step = longest_job.steps[index]
+            # try new step for a better schedule
+            try_new_step(current_step, index, schedule, block_time)
+        # then switch random steps
+        else:
+            # switch random steps
+            job_index = random.randint(0, len(schedule.jobs) - 1)
+            random_job = schedule.jobs[job_index]
+            step_index = random.randint(0, len(random_job.steps) - 1)
+            random_step = random_job.steps[step_index]
+            try_new_step(random_step, step_index, schedule, block_time)
+
         # make schedule as condense as possible
         schedule.gapcheck()
-        print(schedule)
-        last_schedules.append(schedule)
+        # print(schedule)
+
+    return shortest_schedule
 
 
-def try_new_step(current_step, index, schedule):
-    # search till you've got two viable steps to change or there're no more machines
+def try_new_step(current_step, index, schedule, block_time: int):
+    # search till you've got two viable steps to change or there're no more machines (steps?)
     while index >= 0:
         step_before = current_step.parent
-        if step_before is not None and step_before.get_end_time() < current_step.start_time:
+        if step_before is not None and step_before.get_end_time(
+        ) < current_step.start_time:
             # swap start
             current_machine = schedule.machines[current_step.machine_num]
             timeStep_of_current = current_machine.work[current_step.start_time]
-            timeStep_before_current = current_machine.work[current_step.start_time - 1]
+            timeStep_before_current = current_machine.work[
+                current_step.start_time - 1]
             if timeStep_before_current != idle_timeStep:
                 # versuche den gefundenen step zu tauschen
                 # switch steps
-                print("Steps to be switched:")
-                print("work: " + str(timeStep_of_current))
-                print("first: " + str(timeStep_before_current))
-                if schedule.switch_steps(timeStep_before_current, timeStep_of_current, timeStep_before_current, 5):
+                # print("Steps to be switched:")
+                # print("work: " + str(timeStep_of_current))
+                # print("first: " + str(timeStep_before_current))
+                if schedule.switch_steps(timeStep_before_current,
+                                         timeStep_of_current,
+                                         timeStep_before_current, block_time):
                     current_machine.remove_idle_at_end()
                     break
         elif step_before is None and current_step.start_time != 0:
             current_machine = schedule.machines[current_step.machine_num]
             timeStep_of_current = current_machine.work[current_step.start_time]
-            timeStep_before_current = current_machine.work[current_step.start_time - 1]
+            timeStep_before_current = current_machine.work[
+                current_step.start_time - 1]
             if timeStep_before_current != idle_timeStep:
-                schedule.switch_steps(timeStep_before_current, timeStep_of_current, timeStep_of_current, 5)
+                schedule.switch_steps(timeStep_before_current,
+                                      timeStep_of_current, timeStep_of_current,
+                                      block_time)
                 current_machine.remove_idle_at_end()
         current_step = step_before
         index -= 1
